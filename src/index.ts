9b54df4995f5caf5125a319449e8d5ce4afab495
Mohamed Magdy <mmlokas90>
2022-01-12 14:17:47 +0200

    reinstalling express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-const arrow = () => {}
+import express from 'express'
+
+const app = express()
+const port = 3000
+
+app.get('/', (req,res) => {
+    res.send('Hello world')
+})
+
+app.listen(port, () => {
+    console.log('Listening')
+})
